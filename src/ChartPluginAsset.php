3ee5ffc74efcab1315a4c90b8b2ffd136fd6a328
Antonio Ramirez <amigo.cobos@gmail.com>
2015-02-03 13:28:46 +0100

    PSR1-2

--- src/ChartPluginAsset.php
+++ src/ChartPluginAsset.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @link https://github.com/2amigos/yii2-selectize-widget
- * @copyright Copyright (c) 2013 2amigOS! Consulting Group LLC
+ * @copyright Copyright (c) 2013-2015 2amigOS! Consulting Group LLC
  * @license http://opensource.org/licenses/BSD-3-Clause
  */
 namespace dosamigos\chartjs;
