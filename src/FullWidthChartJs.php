a23c8f99b479058adb554998a815f586b03084d4
Brian Pfretzschner <brian.pfretzschner@gmail.com>
2015-04-21 10:07:05 +0200

    FullWidthChartJs widget that extends the ChartJs widget by dynamically setting the width of the canvas to the width of the parent element before continuing with normal ChartJs operation

---
+++ src/FullWidthChartJs.php
@@ -0,0 +1,29 @@
+<?php
+namespace app\widgets;
+
+use dosamigos\chartjs\ChartJs;
+use yii\helpers\Html;
+
+class FullWidthChartJs extends ChartJs
+{
+	protected function registerClientScript()
+	{
+		// Remove width value, will be overwritten anyways
+		unset($this->options['width']);
+		$canvas_id = $this->options['id'];
+
+		// Create the js to figure out the width of the canvas parent and
+		// set it as the canvas width before rendering the chart
+		$js = <<<JS
+(function () {
+	var canvasWidth = $("#{$canvas_id}").parent().width();
+	console.log(canvasWidth);
+	document.getElementById("{$canvas_id}").width = canvasWidth;
+})();
+JS;
+
+		$this->view->registerJs($js);
+
+		parent::registerClientScript();
+	}
+}
